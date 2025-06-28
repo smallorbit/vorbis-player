2fd9b720e7104cdcb030bb97ebabb6f1df609d74
Román M. Pacheco <rmpacheco@gmail.com>
2025-06-27 19:17:11 -0500

    Remove width: 100% from LoadingCard to fix player alignment
    - Removed 'width: 100%' from LoadingCard styled component
    - This was causing the player controls to exceed the playlist width
    - Player controls now properly constrain to their content size
    - Fixes the right-side alignment issue where player extended beyond playlist

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -54,7 +54,6 @@ const LoadingCard = styled(Card)`
   border: 1px solid rgba(115, 115, 115, 0.5);
   border-radius: 0 0 ${({ theme }) => theme.borderRadius.lg} ${({ theme }) => theme.borderRadius.lg};
   border-top: none;
-  width: 100%;
 `;
 
 const SpinIcon = styled.div`
@@ -148,9 +147,7 @@ const ControlButtons = styled.div`
 `;
 
 const ControlButton = styled.button<{ isPlaying?: boolean }>`
-  width: 2.5rem;
-  height: 2.5rem;
-  border-radius: 50%;
+  
   border: none;
   display: flex;
   align-items: center;
@@ -547,25 +544,25 @@ const SpotifyPlayerControls = memo<{
       <ControlButtons>
         <ControlButton onClick={onPrevious}>
           <svg viewBox="0 0 24 24">
-            <path d="M6 6h2v12H6zm3.5 6l8.5 6V6z"/>
+            <path d="M6 6h2v12H6zm3.5 6l8.5 6V6z" />
           </svg>
         </ControlButton>
 
         <ControlButton isPlaying={isPlaying} onClick={handlePlayPause}>
           {isPlaying ? (
             <svg viewBox="0 0 24 24">
-              <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+              <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
             </svg>
           ) : (
             <svg viewBox="0 0 24 24">
-              <path d="M8 5v14l11-7z"/>
+              <path d="M8 5v14l11-7z" />
             </svg>
           )}
         </ControlButton>
 
         <ControlButton onClick={onNext}>
           <svg viewBox="0 0 24 24">
-            <path d="M6 18l8.5-6L6 6v12zM16 6v12h2V6h-2z"/>
+            <path d="M6 18l8.5-6L6 6v12zM16 6v12h2V6h-2z" />
           </svg>
         </ControlButton>
       </ControlButtons>
@@ -574,7 +571,7 @@ const SpotifyPlayerControls = memo<{
       <VolumeSection>
         <VolumeIcon>
           <svg viewBox="0 0 24 24">
-            <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+            <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z" />
           </svg>
         </VolumeIcon>
         <VolumeSlider onClick={handleVolumeClick}>
