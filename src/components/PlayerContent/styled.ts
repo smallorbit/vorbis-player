b07bf37fc74e7aafd9bac9e7183fd480ae274609
Román M. Pacheco <rmpacheco@gmail.com>
2026-06-25 23:32:24 -0500

    Display album name in zen mode track info (#1668)

--- src/components/PlayerContent/styled.ts
+++ src/components/PlayerContent/styled.ts
@@ -193,6 +193,19 @@ export const ZenTrackName = styled.div.withConfig({
   width: 100%;
 `;
 
+export const ZenTrackAlbum = styled.div`
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  line-height: 1.4;
+  color: ${({ theme }) => theme.colors.gray[400]};
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  letter-spacing: 0.02em;
+  text-shadow: ${({ theme }) => theme.shadows.textSm};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+`;
+
 export const ZenTrackArtist = styled.div`
   font-size: ${({ theme }) => theme.fontSize.base};
   line-height: 1.4;
